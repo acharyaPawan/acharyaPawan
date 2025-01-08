a9190ae4e5304acc6c4d84ddd0174bd8c8da8964
Pawan <not@specified.com>
2025-01-09 01:55:43 +0545

    configured

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        My Portfolio
+      </h1>
+      <p className="mb-4">
+        {`I'm a Fullstack developer. This repo is in progress.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
